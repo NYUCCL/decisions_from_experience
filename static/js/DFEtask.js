0e3afe5e9290407466da525779e7602f22ef21fe
Alex Rich <alexandersrich@gmail.com>
2017-05-30 16:20:39 -0400

    assembling old code changes so it halfway works...

--- static/js/DFEtask.js
+++ static/js/DFEtask.js
@@ -5,71 +5,75 @@
  */
 
 /*jslint browser: true*/
-/*global $, jQuery, alert, PsiTurk, uniqueId, adServerLoc, mode, condition, counterbalance */
+/*global $, window, setTimeout, clearInterval, PsiTurk, uniqueId, adServerLoc, mode, condition, counterbalance */
 
 // Initalize psiturk object
 var psiTurk = new PsiTurk(uniqueId, adServerLoc, mode),
-    mycondition = condition,
-    mycounterbalance = counterbalance,
-    currentview;
-// they are not used in the stroop code but may be useful to you
-
-// All pages to be loaded
-var pages = [
-    "instructions/instruct-ready.html",
-    "stage.html",
-    "postquestionnaire.html"
-];
+    EXP = {
+        "condition": condition,
+        "counterbalance": counterbalance,
+        "pages": [
+            "instructions/instruct-ready.html",
+            "stage.html",
+            "postquestionnaire.html"
+        ]
+    };
 
 
-psiTurk.preloadPages(pages);
+psiTurk.preloadPages(EXP.pages);
 
 var instructionPages = [ // add as a list as many pages as you like
     "instructions/instruct-ready.html"
 ];
 
 
-function MoneyMachine(machine, responseFn) {
+function bandit(specs, responseFn) {
     "use strict";
     var makeOutcome = function () {
         var r = Math.random(),
             outcome;
-        if (r < machine.probability) {
-            outcome = machine.payoff;
+        if (r < specs.probability) {
+            outcome = specs.payoff;
         } else {
             outcome = 0;
         }
-        $('#machine' + machine.machineId.toString() + 'outcome').html('Most recent outcome: ' +
+        $("#machine" + specs.banditId.toString() + "outcome").html("Most recent outcome: " +
                                                                outcome.toString());
-        responseFn(machine.machineId, outcome);
+        responseFn(specs.banditId, outcome);
     };
-    $('#machine' + machine.machineId.toString()).click(makeOutcome);
+    $("#machine" + specs.banditId.toString()).click(makeOutcome);
 }
 
-function PayoffCounter(startingNum) {
+function payoffCounter(startingNum) {
     "use strict";
-    this.payoff = startingNum;
-    this.credit = function (change) {
-        this.payoff += change;
-        $('#bonus').html('current payoff: ' + this.payoff.toString());
+    var payoff = startingNum,
+        that = {};
+    that.credit = function (change) {
+        payoff += change;
+        $("#bonus").html("current payoff: " + payoff.toString());
     };
-    this.getPayoff = function () {
-        return this.payoff;
+    that.getPayoff = function () {
+        return payoff;
     };
-    this.credit(0);
+    that.credit(0);
+    return that;
 }
 
 
-function DecisionProblem(problem, completeFn) {
+function decisionProblem(problem, completeFn) {
     "use strict";
-    psiTurk.showPage('stage.html');
+    psiTurk.showPage("stage.html");
     var sampleCost = problem.sampleCost,
         samplePhase = true,
-        totalSamples = {machine1: 0, machine2: 0},
-        bank = new PayoffCounter(0);
+        totalSamples = {bandit1: 0, bandit2: 0},
+        bank = payoffCounter(0);
+
+    function finish() {
+        completeFn();
+    }
 
     function responseFn(button, outcome) {
-        // button code 0 indicates participant is ready to choose a machine
+        // button code 0 indicates participant is ready to choose a bandit
         if (button === 0) {
             samplePhase = false;
         } else {
@@ -79,14 +83,14 @@ function DecisionProblem(problem, completeFn) {
             } else {
                 bank.credit(outcome);
             }
-            psiTurk.recordTrialData({'sample': samplePhase,
-                                     'machine': button,
-                                     'outcome': outcome,
-                                     'bankAfter': bank.getPayoff(),
-                                     'sampleCost': sampleCost,
-                                     'totalSamples': totalSamples.machine1 + totalSamples.machine2,
-                                     'samples1': totalSamples.machine1,
-                                     'samples2': totalSamples.machine2
+            psiTurk.recordTrialData({"sample": samplePhase,
+                                     "banditId": button,
+                                     "outcome": outcome,
+                                     "bankAfter": bank.getPayoff(),
+                                     "sampleCost": sampleCost,
+                                     "totalSamples": totalSamples.bandit1 + totalSamples.bandit2,
+                                     "samples1": totalSamples.bandit1,
+                                     "samples2": totalSamples.bandit2
                                     });
 
             if (!samplePhase) {
@@ -96,31 +100,82 @@ function DecisionProblem(problem, completeFn) {
 
     }
 
-    new MoneyMachine(problem.machine1, responseFn);
-    new MoneyMachine(problem.machine2, responseFn);
+    bandit(problem.bandit1, responseFn);
+    bandit(problem.bandit2, responseFn);
 
-    function finish() {
-        completeFn();
-    }
-
-    $('#decision').click(function () {responseFn(0, 0); });
+    $("#decision").click(function () {responseFn(0, 0); });
 
 }
 
-function ExperimentDriver() {
+function questionnaire() {
     "use strict";
-    var self = this,
-        allProblems;
+    var errorMessage = "<h1>Oops!</h1><p>Something went wrong submitting your HIT. This might happen if you lose your internet connection. Press the button to resubmit.</p><button id='resubmit'>Resubmit</button>",
+        replaceBody, promptResubmit, resubmit, recordResponses;
 
-    this.runNext = function () {
+    replaceBody = function(x) { $("body").html(x); };
+    recordResponses = function () {
+        psiTurk.recordTrialData({"phase": "postquestionnaire", "status": "submit"});
+
+        $("textarea").each(function () {
+            psiTurk.recordUnstructuredData(this.id, this.value);
+        });
+        $("select").each(function () {
+            psiTurk.recordUnstructuredData(this.id, this.value);
+        });
+    };
+
+    promptResubmit = function () {
+        replaceBody(errorMessage);
+        $("#resubmit").click(resubmit);
+    };
+
+    resubmit = function () {
+        var reprompt;
+        replaceBody("<h1>Trying to resubmit...</h1>");
+        reprompt = setTimeout(promptResubmit, 10000);
+
+        psiTurk.saveData({
+            success: function() {
+                clearInterval(reprompt);
+                // psiTurk.computeBonus("compute_bonus", function () {
+                //     psiTurk.completeHIT();
+                // });
+                psiTurk.completeHIT();
+            },
+            error: promptResubmit
+        });
+    };
+
+    // Load the questionnaire snippet
+    psiTurk.showPage("postquestionnaire.html");
+    psiTurk.recordTrialData({"phase": "postquestionnaire", "status": "begin"});
+
+    $("#next").click(function () {
+        recordResponses();
+        psiTurk.saveData({
+            success: function () {
+                // psiTurk.computeBonus("compute_bonus", function () {
+                psiTurk.completeHIT(); // when finished saving compute bonus, the quit
+                // });
+            },
+            error: promptResubmit});
+    });
+
+}
+
+function experimentDriver() {
+    "use strict";
+    var allProblems, runNext;
+
+    runNext = function () {
         var completeFn,
             problem = allProblems.shift();
         if (allProblems.length === 0) {
-            completeFn = function () {currentview = new Questionnaire(); };
+            completeFn = function () {questionnaire(); };
         } else {
-            completeFn = function () {self.runNext(); };
+            completeFn = function () {runNext(); };
         }
-        currentview = new DecisionProblem(problem, completeFn);
+        decisionProblem(problem, completeFn);
     };
 
     $.ajax({
@@ -128,72 +183,23 @@ function ExperimentDriver() {
         url: "/get_stims",
         data: {condition: condition,
                counterbalance: counterbalance
-               },
+              },
         success: function (data) {
             allProblems = data.results;
-            self.runNext();
+            runNext();
         }
     });
 
 }
 
 
-function Questionnaire() {
-
-    var error_message = "<h1>Oops!</h1><p>Something went wrong submitting your HIT. This might happen if you lose your internet connection. Press the button to resubmit.</p><button id='resubmit'>Resubmit</button>";
-
-    var record_responses = function () {
-        psiTurk.recordTrialData({'phase': 'postquestionnaire', 'status': 'submit'});
-
-        $('textarea').each( function(i, val) {
-            psiTurk.recordUnstructuredData(this.id, this.value);
-        });
-        $('select').each( function(i, val) {
-            psiTurk.recordUnstructuredData(this.id, this.value);
-        });
-    };
-
-    var prompt_resubmit = function() {
-        replaceBody(error_message);
-        $("#resubmit").click(resubmit);
-    };
-
-    resubmit = function() {
-        replaceBody("<h1>Trying to resubmit...</h1>");
-        reprompt = setTimeout(prompt_resubmit, 10000);
-
-        psiTurk.saveData({
-            success: function() {
-                clearInterval(reprompt);
-                psiTurk.computeBonus('compute_bonus', function () {finish(); });
-            },
-            error: prompt_resubmit
-        });
-    };
-
-    // Load the questionnaire snippet
-    psiTurk.showPage('postquestionnaire.html');
-    psiTurk.recordTrialData({'phase':'postquestionnaire', 'status':'begin'});
-
-    $("#next").click(function () {
-        record_responses();
-        psiTurk.saveData({
-            success: function () {
-                psiTurk.computeBonus('compute_bonus', function () {
-                    psiTurk.completeHIT(); // when finished saving compute bonus, the quit
-                });
-            },
-            error: prompt_resubmit});
-    });
-
-};
-
 /*******************
  * Run Task
  ******************/
 $(window).load(function () {
+    "use strict";
     psiTurk.doInstructions(
         instructionPages, // a list of pages you want to display in sequence
-    	function () { currentview = new ExperimentDriver(); }
+        function () { experimentDriver(); }
     );
 });
--- stimuli_generator.py
+++ stimuli_generator.py
@@ -1,18 +1,18 @@
 def problems_generator(condition, counterbalance):
     exp_definition = [{'problemNum': 1,
                        'sampleCost': -0.1,
-                       'machine1': {'machineId': 1,
+                       'bandit1': {'banditId': 1,
                                   'payoff': 3,
                                   'probability': 0.25},
-                       'machine2': {'machineId': 2,
+                       'bandit2': {'banditId': 2,
                                   'payoff': 4,
                                   'probability': 0.2}},
                       {'problemNum': 2,
                        'sampleCost': -0.1,
-                       'machine1': {'machineId': 1,
+                       'bandit1': {'banditId': 1,
                                   'payoff': 3,
                                   'probability': 1},
-                       'machine2': {'machineId': 2,
+                       'bandit2': {'banditId': 2,
                                   'payoff': 8,
                                   'probability': 1}}
                       ]
